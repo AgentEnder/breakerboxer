789d313278b5aa6c090e822449db14238de76d23
Craigory Coppola <craigory.coppola@universalplant.com>
2020-11-24 20:56:00 -0500

    Update angular to v11

--- src/app/workspace-page/sidebar-help/sidebar-help.component.spec.ts
+++ src/app/workspace-page/sidebar-help/sidebar-help.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SidebarHelpComponent } from './sidebar-help.component';
 
@@ -6,7 +6,7 @@ describe('SidebarHelpComponent', () => {
   let component: SidebarHelpComponent;
   let fixture: ComponentFixture<SidebarHelpComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SidebarHelpComponent ]
     })
