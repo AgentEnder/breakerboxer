cb1f93ab103355ee1bd1951ba115f272f51ccf18
Craigory Coppola <craigory.coppola@universalplant.com>
2021-01-24 21:11:35 -0600

    feat(emu-compat): rating support for ngModel / value accessor
    Signed-off-by: Craigory Coppola <craigory.coppola@universalplant.com>

--- libs/rating/src/lib/base-components/rating.base-component.ts
+++ libs/rating/src/lib/base-components/rating.base-component.ts
@@ -1,8 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Directive, EventEmitter, Input, Output } from '@angular/core';
+import {ControlValueAccessor} from '@angular/forms';
 import { Rating } from '../models/rating.interface';
 
-@Component({ template: '' })
-export class RatingBaseComponent {
+@Directive()
+// tslint:disable-next-line: directive-class-suffix
+export abstract class RatingBaseComponent implements ControlValueAccessor {
   private _rating: Rating;
   @Input() set rating(v: Rating) {
     this._rating = v;
@@ -12,9 +14,25 @@ export class RatingBaseComponent {
     return this._rating;
   }
 
-  display: number;
+  private _value: any;
 
-  @Output() ratingSelected = new EventEmitter<number>();
+  set value(value: number) {
+    this._value = value;
+    this.rating = {
+      ...this.rating,
+      mine: value
+    };
+    this.notifyValueChange();
+  }
+
+  get value(): number {
+    return this._value;
+  }
+
+  onChange: (value) => {};
+  onTouched: () => {};
+
+  display: number;
 
   rate(val: number): void {
     if (this._rating.mine === null) {
@@ -22,6 +40,28 @@ export class RatingBaseComponent {
     }
     this._rating.mine = val;
     this.display = val;
-    this.ratingSelected.emit(val);
+    this.value = val;
+  }
+
+  notifyValueChange(): void {
+    if (this.onChange) {
+      this.onChange(this.value);
+    }
+  }
+
+  writeValue(obj: any): void {
+    this._value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
   }
 }
+
--- libs/web/rating/src/lib/rating/rating.component.ts
+++ libs/web/rating/src/lib/rating/rating.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { RatingBaseComponent } from '@tbs/rating';
+
+export const WEB_RATING_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => RatingComponent),
+  multi: true
+};
 
 @Component({
   selector: 'tbs-rating',
   templateUrl: './rating.component.html',
-  styleUrls: ['./rating.component.scss']
+  styleUrls: ['./rating.component.scss'],
+  providers: [WEB_RATING_VALUE_ACCESSOR]
 })
 export class RatingComponent extends RatingBaseComponent implements OnInit {
 
