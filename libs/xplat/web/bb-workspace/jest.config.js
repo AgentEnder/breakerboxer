d5268126eed5be110e90e373916191fe9ac3f5bd
Craigory Coppola <craigory.coppola@universalplant.com>
2021-02-27 12:10:47 -0600

    some tslint fixes

--- libs/xplat/web/bb-workspace/jest.config.js
+++ libs/xplat/web/bb-workspace/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'web-bb-workspace',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
