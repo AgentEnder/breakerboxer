5f61bb4f0d7c5935e35b0f4884c27f916a1c7817
Craigory Coppola <craigory.coppola@universalplant.com>
2021-02-27 14:04:18 -0600

    portfolio environment setup

--- apps/web-portfolio/src/app/app.module.ts
+++ apps/web-portfolio/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 
-// libs
-import { environment } from '../environments/environment';
+import { LayoutModule } from '@tbs/layout';
 
-// app
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './features/shared/shared.module';
-import { AppRoutingModule } from './app.routing';
-import { AppComponent } from './app.component';
-import { LayoutModule } from '@tbs/layout';
 
 @NgModule({
   imports: [CoreModule, SharedModule, LayoutModule, AppRoutingModule],
