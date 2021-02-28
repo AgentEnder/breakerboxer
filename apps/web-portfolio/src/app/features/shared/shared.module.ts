07493c106d3502928ccaaef4d1fbdea8750d132f
Craigory Coppola <craigory.coppola@universalplant.com>
2021-02-28 17:05:33 -0600

    feat(portfolio): beginning of skills tab

--- apps/web-portfolio/src/app/features/shared/shared.module.ts
+++ apps/web-portfolio/src/app/features/shared/shared.module.ts
@@ -4,17 +4,27 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { LayoutModule } from '@tbs/layout';
 import { UIModule } from '@tbs/xplat/web/features';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 import { LanguageLogoComponent } from './language-logo/language-logo.component';
 
-const MODULES = [UIModule, LayoutModule, CommonModule, MatButtonModule, MatCardModule, MatIconModule];
+const MODULES = [
+  UIModule,
+  LayoutModule,
+  CommonModule,
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatTooltipModule,
+];
 
 @NgModule({
   imports: [...MODULES],
-  declarations: [LanguageLogoComponent],
-  exports: [...MODULES, LanguageLogoComponent],
+  declarations: [LanguageLogoComponent, ContactInfoComponent],
+  exports: [...MODULES, LanguageLogoComponent, ContactInfoComponent],
 })
 export class SharedModule {}
